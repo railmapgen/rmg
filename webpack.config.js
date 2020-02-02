5013a6c114c765f686d3389039bf03e7f4be4ff6
Chito Wong <9390786+wongchito@users.noreply.github.com>
2020-02-03 03:11:28 +0800

    (V2.4): fix relative path

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   output: {
     filename: '[name].min.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    publicPath: './dist/'
   }, 
   optimization: {
     splitChunks: {
