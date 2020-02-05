a23c167f5771654e6c795f2b0aa6c5ce764ac5af
thekingofcity <wzl7777777@gmail.com>
2020-02-05 22:13:11 +0800

    svg#destination almost done

--- src/Line/LineSH.ts
+++ src/Line/LineSH.ts
@@ -18,6 +18,7 @@ export class RMGLineSH extends RMGLine {
         }
     }
 
+    // draw the line in railmap
     drawLine() {
         $('.rmg-line').removeClass('rmg-line__mtr').addClass('rmg-line__shmetro');
 
@@ -75,6 +76,63 @@ export class RMGLineSH extends RMGLine {
         // pass stroke should be added somewhere else
         // but I can't figure it out
         $('path#stn_sh_pass').attr('stroke', '#aaa');
+
+        // the last decoration line
+        $('#line_shmetro_left_use').attr('fill', this._themeColour)
+    }
+
+    drawDestInfo() {
+        $('#station_info_shmetro > #platform > text').text(this._platformNum);
+
+        let validDest: ID[] = this[this._direction + 'ValidDests'];
+
+        var [destNameZH, destNameEN] = [0, 1].map(idx => {
+            return validDest.map(stnId => this.stations[stnId].name[idx].replace(/\\/g, ' ')).join('/');
+        })
+
+        var bcr = $('#station_info_shmetro > g:last-child')[0].getBoundingClientRect();
+        var flagLength = 160 + 150 + bcr.width + 45 + 50;
+        console.log("flagLength: " + flagLength)
+
+
+
+        // arrow
+        var isLeft = (this._direction == 'r') ? 1 : -1;
+        var arrowX = (this._svgDestWidth - isLeft * flagLength) / 20;
+        var arrowRotate = 90 * (1 - isLeft);
+        $('#station_info_shmetro > #arrow_left_use').attr('transform', `translate(${arrowX},135)rotate(${arrowRotate})`);
+
+        // not in use now
+        var platformNumX = arrowX + isLeft * (160 + 50 + 75);
+        $('#station_info_shmetro > #platform').attr('transform', `translate(${platformNumX},130)`);
+
+        // list the destination text
+        // Todo: fix svg_dest_width*0.8, this has only been tested on 1000 width
+        if (this._direction === 'l') {
+            var txtAnchor = 'end';
+            var destNameX = this._svgDestWidth * 0.8;
+        } else {
+            var txtAnchor = 'start';
+            var destNameX = this._svgDestWidth * 0.2;
+        }
+        $('#station_info_shmetro > g:last-child').attr({
+            transform: `translate(${destNameX},135)`,
+            'text-anchor': txtAnchor
+        });
+
+        // for each left valid destinations, get the name from id
+        var destinations_zh = "", destinations_en = ""
+        this.lValidDests.forEach(stn => {
+            destinations_zh += this.stations[stn].name[0]
+            destinations_en += this.stations[stn].name[1]
+        });
+        $('#station_info_shmetro > #dest_text > text:first-child').text(`往${destinations_zh}`)
+        $('#station_info_shmetro > #dest_text > text:last-child').text(`To ${destinations_en}`)
+
+        // the last decoration line
+        $('#line_shmetro_left_use').attr({
+            transform: `translate(0,220)`,
+        })
     }
 
     updateStnNameBg() {
