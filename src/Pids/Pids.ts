907a45bc2a21dc6c22966b2b70d565eb1dc25925
thekingofcity <wzl7777777@gmail.com>
2020-02-21 20:21:02 +0800

    pids createTimeTable

--- src/Pids/Pids.ts
+++ src/Pids/Pids.ts
@@ -4,8 +4,8 @@ export class Pids {
     protected _t?: number
     protected _duration?: number
     protected _frameRate?: number
-    protected _start?: string
-    protected _destination?: string
+    protected _start: string
+    protected _end: string
     protected _timeTable: {
         [stnId: string]: [number, number]
     } = {}
@@ -13,6 +13,8 @@ export class Pids {
 
     constructor(line: RMGLine) {
         this._line = line
+        this._start = this._line.routes[0].filter(stnId => stnId !== 'lineend' && stnId !== 'linestart')[0]
+        this._end =  this._line.routes[0].filter(stnId => stnId !== 'lineend' && stnId !== 'linestart').reverse()[0]
     }
 
     /**
@@ -26,16 +28,34 @@ export class Pids {
         return (textsInt[0] * 3600 + textsInt[1] * 60 + textsInt[2]) * this._frameRate + textsInt[4]
     }
 
-    set start(stnId: string) {
-        if (stnId in this._line.stations) {
-            this._start = stnId
-        }
+    protected _createTimeTable() {
+        this._line.routes
+            .map(route => {
+                let start = route.indexOf(this._start)
+                let end = route.indexOf(this._end)
+
+                // not in this route
+                if (!start || !end) return
+
+                // swap if start is bigger than end to be used in slice
+                if (start > end) [start, end] = [end, start]
+
+                // get the part where the train travels
+                route = route.slice(start, end + 1)
+
+                // update the time table
+                route.map(stnId => this._timeTable[stnId] = [0, 0])
+            })
     }
 
-    set destination(stnId: string) {
-        if (stnId in this._line.stations) {
-            this._destination = stnId
-        }
+    set start(stnId: string) {
+        this._start = stnId
+        if (this._end) this._createTimeTable()
+    }
+
+    set end(stnId: string) {
+        this._end = stnId
+        if (this._start) this._createTimeTable()
     }
 
     set frameRate(val: string) {
@@ -47,7 +67,7 @@ export class Pids {
     set duration(val: string) {
         this._duration = this._getFrame(val)
     }
-    
+
     set t(val: string) {
         this._t = this._getFrame(val)
 
