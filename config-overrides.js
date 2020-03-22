3f10529fef645370900568ef255855448893625f
Chito Wong <9390786+wongchito@users.noreply.github.com>
2020-03-22 23:26:46 +0800

    update(env): update environment to CRA setup;

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+module.exports = function override(config, env) {
+    config.externals = {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        i18next: 'i18next',
+        'react-i18next': 'ReactI18next',
+        'i18next-xhr-backend': 'i18nextXHRBackend',
+        'i18next-browser-languagedetector': 'i18nextBrowserLanguageDetector',
+        '@material-ui/core': 'MaterialUI',
+    };
+    return config;
+};
