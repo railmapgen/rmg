cfc8d1205a4c4df0ea76c35be33091fb92777347
PassionPenguin <penguin.zhang@qq.com>
2020-04-01 19:56:32 +0800

    add barcelona madrid's metro support Cercanías will be added later

---
+++ src/panels/colour-diag/colours/barcelona.ts
@@ -0,0 +1,106 @@
+const Barcelona: LineEntry[] = [
+    {
+        id: 'bc1',
+        name: {
+            en: 'Line 1',
+            'zh-Hans': '1号线',
+            'zh-Hant': '1號線',
+        },
+        colour: '#DC241F'
+    },
+    {
+        id: 'bc2',
+        name: {
+            en: 'Line 2',
+            'zh-Hans': '2号线',
+            'zh-Hant': '2號線',
+        },
+        colour: '#95288F',
+    },
+    {
+        id: 'bc3',
+        name: {
+            en: 'Line 3',
+            'zh-Hans': '3号线',
+            'zh-Hant': '3號線',
+        },
+        colour: '#1EB53A'
+    },
+    {
+        id: 'bc4',
+        name: {
+            en: 'Line 4',
+            'zh-Hans': '4号线',
+            'zh-Hant': '4號線',
+        },
+        colour: '#EAAF0F',
+        fg: '#000',
+    },
+    {
+        id: 'bc5',
+        name: {
+            en: 'Line 5',
+            'zh-Hans': '5号线',
+            'zh-Hant': '5號線',
+        },
+        colour: '#0247FE',
+    },
+    {
+        id: 'bc6',
+        name: {
+            en: 'Line 6',
+            'zh-Hans': '6号线',
+            'zh-Hant': '6號線',
+        },
+        colour: '#8977BA',
+    },
+    {
+        id: 'bc7',
+        name: {
+            en: 'Line 7',
+            'zh-Hans': '7号线',
+            'zh-Hant': '7號線',
+        },
+        colour: '#9B4F19',
+    },
+    {
+        id: 'bc8',
+        name: {
+            en: 'Line 8',
+            'zh-Hans': '8号线',
+            'zh-Hant': '8號線',
+        },
+        colour: '#F484C4',
+        fg: '#000',
+    },
+    {
+        id: 'bc9',
+        name: {
+            en: 'Line 9',
+            'zh-Hans': '9号线',
+            'zh-Hant': '9號線',
+        },
+        colour: '#FF6600'
+    },
+    {
+        id: 'bc10',
+        name: {
+            en: 'Line 10',
+            'zh-Hans': '10号线',
+            'zh-Hant': '10號線',
+        },
+        colour: '#007FFF',
+    },
+    {
+        id: 'bc11',
+        name: {
+            en: 'Line 11',
+            'zh-Hans': '11号线',
+            'zh-Hant': '11號線',
+        },
+        colour: '#87D300',
+        fg: '#000',
+    }
+];
+
+export default Barcelona;
---
+++ src/panels/colour-diag/colours/madrid.ts
@@ -0,0 +1,150 @@
+const Madrid: LineEntry[] = [
+    {
+        id: 'md1',
+        name: {
+            en: 'Line 1',
+            'zh-Hans': '1号线',
+            'zh-Hant': '1號線',
+        },
+        colour: '#2ca5dd',
+    },
+    {
+        id: 'md2',
+        name: {
+            en: 'Line 2',
+            'zh-Hans': '2号线',
+            'zh-Hant': '2號線',
+        },
+        colour: '#e1252b',
+        // fg: '000080', => ignore foreground
+    },
+    {
+        id: 'md3',
+        name: {
+            en: 'Line 3',
+            'zh-Hans': '3号线',
+            'zh-Hant': '3號線',
+        },
+        colour: '#ffe20b',
+        fg: '000080',
+    },
+    {
+        id: 'md4',
+        name: {
+            en: 'Line 4',
+            'zh-Hans': '4号线',
+            'zh-Hant': '4號線',
+        },
+        colour: '#823f02',
+    },
+    {
+        id: 'md5',
+        name: {
+            en: 'Line 5',
+            'zh-Hans': '5号线',
+            'zh-Hant': '5號線',
+        },
+        colour: '#97c104',
+    },
+    {
+        id: 'md6',
+        name: {
+            en: 'Line 6 Circle',
+            'zh-Hans': '6号线 环线',
+            'zh-Hant': '6環線 環線',
+        },
+        colour: '#9c9b9b',
+    },
+    {
+        id: 'md7',
+        name: {
+            en: 'Line 7',
+            'zh-Hans': '7号线',
+            'zh-Hant': '7號線',
+        },
+        colour: '#f96608',
+    },
+    {
+        id: 'md8',
+        name: {
+            en: 'Line 8',
+            'zh-Hans': '8号线',
+            'zh-Hant': '8號線',
+        },
+        colour: '#f373b9'
+    },
+    {
+        id: 'md9',
+        name: {
+            en: 'Line 9',
+            'zh-Hans': '9号线',
+            'zh-Hant': '9號線',
+        },
+        colour: '#9a0466',
+    },
+    {
+        id: 'md10',
+        name: {
+            en: 'Line 10',
+            'zh-Hans': '10号线',
+            'zh-Hant': '10號線',
+        },
+        colour: '#140381',
+    },
+    {
+        id: 'md11',
+        name: {
+            en: 'Line 11',
+            'zh-Hans': '11号线',
+            'zh-Hant': '11號線',
+        },
+        colour: '#0b6921',
+    },
+    {
+        id: 'md12',
+        name: {
+            en: 'Line 12',
+            'zh-Hans': '12号线',
+            'zh-Hant': '12號線',
+        },
+        colour: '#9a9b30',
+    },
+    {
+        id: 'mdr',
+        name: {
+            en: 'Ramal Line',
+            'zh-Hans': '支线',
+            'zh-Hant': '支線',
+        },
+        colour: '#140381',
+    },
+    {
+        id: 'mdl1',
+        name: {
+            en: 'LightRail Line 1',
+            'zh-Hans': '轻铁1号线',
+            'zh-Hant': '輕鐵1號線',
+        },
+        colour: '#3a7cda',
+    },
+    {
+        id: 'mdl2',
+        name: {
+            en: 'LightRail Line 2',
+            'zh-Hans': '轻铁2号线',
+            'zh-Hant': '輕鐵2號線',
+        },
+        colour: '#a50084',
+    },
+    {
+        id: 'mdl3',
+        name: {
+            en: 'LightRail Line 3',
+            'zh-Hans': '轻铁3号线',
+            'zh-Hant': '輕鐵3號線',
+        },
+        colour: '#ee1c25',
+    },
+];
+
+export default Madrid;
