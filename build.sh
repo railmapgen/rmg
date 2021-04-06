0d630c2e15ac2090be7e5c486d059729b7468f02
Chito Wong <9390786+wongchito@users.noreply.github.com>
2021-04-06 23:53:00 +0800

    #72 Update GitHub workflow

--- build.sh
+++ build.sh
@@ -4,17 +4,22 @@ set -x
 export APP_NAME=rmg
 BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 
+git config --global user.name "Build Agent"
+git config --global user.email rmg.build.agent@users.noreply.github.com
+
 if [ "$BRANCH" = "master" ]
 then
   # build with a normal version
-  npm version patch -m "%s release" --force || { echo "Release Error"; exit 1; }
+  npm version patch -m "${APP_NAME}-%s release" --force || { echo "Release Error"; exit 1; }
   export RELEASE_VERSION=$(node -p "require('./package.json').version")
-  git tag -a "${APP_NAME}-${RELEASE_VERSION}" -m "${APP_NAME}-${RELEASE_VERSION}"
-  git push origin HEAD
-  git push origin "${APP_NAME}-${RELEASE_VERSION}"
+#  git tag -a "${APP_NAME}-${RELEASE_VERSION}" -m "${APP_NAME}-${RELEASE_VERSION}"
 else
   # build with a hashed version
   VERSION=`node -p "require('./package.json').version"`
   GITHASH=$(git log -n 1 --pretty=%h)
   export RELEASE_VERSION="$VERSION.$BRANCH.$GITHASH"
+  git tag -a "${APP_NAME}-${RELEASE_VERSION}" -m "${APP_NAME}-${RELEASE_VERSION} release"
 fi
+
+git push origin HEAD
+git push origin "${APP_NAME}-${RELEASE_VERSION}"
