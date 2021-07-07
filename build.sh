47c9b14c2f9f7c0b558ee00dc9a363e73d5bddeb
Chito Wong <9390786+wongchito@users.noreply.github.com>
2021-07-07 18:41:15 +0800

    #NOISSUE Update build script

--- build.sh
+++ build.sh
@@ -18,32 +18,17 @@ if [ "$BRANCH" = "master" ]
 then
   # build with a normal version
   npm version patch -m "${APP_NAME}-%s release" --force || { echo "Release Error"; exit 1; }
-  export RELEASE_VERSION=$(node -p "require('./package.json').version")
-#  git tag -a "${APP_NAME}-${RELEASE_VERSION}" -m "${APP_NAME}-${RELEASE_VERSION} release"
+  export RMG_VER=$(node -p "require('./package.json').version")
 else
   # build with a hashed version
   VERSION=`node -p "require('./package.json').version"`
   GITHASH=$(git log -n 1 --pretty=%h)
-  export RELEASE_VERSION="$VERSION.$BRANCH.$GITHASH"
-  git tag -a "${APP_NAME}-${RELEASE_VERSION}" -m "${APP_NAME}-${RELEASE_VERSION}"
+  export RMG_VER="$VERSION.$BRANCH.$GITHASH"
+  git tag -a "${APP_NAME}-${RMG_VER}" -m "${APP_NAME}-${RMG_VER}"
 fi
 
-# build PRD
+# build PRD and copy artifact to repository
 CI='' npm run build
-
-# push tag and commit
-if [ "$BRANCH" = "master" ]
-then
-  git push origin HEAD
-  git push origin "${APP_NAME}-${RELEASE_VERSION}"
-else
-  git push origin "${APP_NAME}-${RELEASE_VERSION}"
-fi
-
-#echo "RMG_VER=${RELEASE_VERSION}" >> $GITHUB_ENV
-RMG_VER=$RELEASE_VERSION
-
-# copy PRD artifact to repository
 mkdir $UAT_REPO_NAME/$RMG_VER/
 cp -r build/ $UAT_REPO_NAME/$RMG_VER/PRD/
 
@@ -52,6 +37,13 @@ cat package.json | sed '2 s/RailMapGenerator/uat-rail-map-generator/' > package-
 cp package-new.json package.json
 CI='' npm run build
 cp -r build/ $UAT_REPO_NAME/$RMG_VER/UAT/
+
+# push tag and commit
+if [ "$BRANCH" = "master" ]
+then
+  git push origin HEAD
+fi
+git push origin "${APP_NAME}-${RMG_VER}"
 
 # upload artifacts
 cd $UAT_REPO_NAME/
