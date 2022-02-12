816a7d4c34272eb8799ef124b5ad79d2230e8a1e
Chito Wong <chi.to.wong@outlook.com>
2022-02-13 02:23:09 +0800

    #200 Fix unit tests

--- src/components/page-header/open-actions.test.tsx
+++ src/components/page-header/open-actions.test.tsx
@@ -4,6 +4,8 @@ import OpenActions from './open-actions';
 import { createMockAppStore, TestingProvider } from '../../setupTests';
 import rootReducer from '../../redux';
 import { SET_GLOBAL_ALERT } from '../../redux/app/action';
+import * as utils from '../../util/utils';
+import { act } from 'react-dom/test-utils';
 
 jest.mock('../../index', () => ({
     reRenderApp: jest.fn(),
@@ -16,7 +18,9 @@ global.window.rmgStorage = {
 const realStore = rootReducer.getState();
 const mockStore = createMockAppStore(realStore);
 
-const mockUploadedFile = new File(['{style: "mtr"}'], 'mock-uploaded-file.json', { type: 'application/json' });
+const readFileAsTextSpy = jest.spyOn(utils, 'readFileAsText');
+
+const mockUploadedFile = new File([], 'mock-uploaded-file.json', { type: 'application/json' });
 const mockInvalidTypeFile = new File(['dummy-content'], 'invalid-file-type.txt', { type: 'text/plain' });
 const mockMalFormatFile = new File(['random-content'], 'mal-format-file.json', { type: 'application/json' });
 
@@ -26,20 +30,22 @@ describe('Unit tests for OpenActions component', () => {
     });
 
     it('Can open UploadConfirmModal if a valid file is uploaded', async () => {
+        readFileAsTextSpy.mockResolvedValue(JSON.stringify({ stn_list: [], theme: [] }));
         const wrapper = mount(<OpenActions />, { wrappingComponent: TestingProvider });
 
         // modal is closed
         expect(document.querySelectorAll('.chakra-portal')).toHaveLength(0);
 
-        await wrapper.find('input[type="file"]').simulate('change', { target: { files: [mockUploadedFile] } });
-        wrapper.update();
+        await act(async () => {
+            await wrapper.find('input[type="file"]').simulate('change', { target: { files: [mockUploadedFile] } });
+        });
 
-        // FIXME: why file is not loaded
         // modal open
-        // expect(document.querySelectorAll('.chakra-portal')).toHaveLength(1);
+        expect(document.querySelectorAll('.chakra-portal')).toHaveLength(1);
     });
 
     it('Can display error message if invalid type of file is uploaded', async () => {
+        readFileAsTextSpy.mockResolvedValue('dummy-content');
         const wrapper = mount(<OpenActions />, {
             wrappingComponent: TestingProvider,
             wrappingComponentProps: { store: mockStore },
@@ -59,22 +65,24 @@ describe('Unit tests for OpenActions component', () => {
     });
 
     it('Can display error message if mal format file is uploaded', async () => {
+        readFileAsTextSpy.mockResolvedValue('random-content');
         const wrapper = mount(<OpenActions />, {
             wrappingComponent: TestingProvider,
             wrappingComponentProps: { store: mockStore },
         });
 
-        await wrapper.find('input[type="file"]').simulate('change', { target: { files: [mockMalFormatFile] } });
+        await act(async () => {
+            await wrapper.find('input[type="file"]').simulate('change', { target: { files: [mockMalFormatFile] } });
+        });
 
         const actions = mockStore.getActions();
-        // FIXME: why file is not loaded
-        // expect(actions).toHaveLength(1);
-        // expect(actions).toContainEqual({
-        //     type: SET_GLOBAL_ALERT,
-        //     globalAlert: {
-        //         status: 'error',
-        //         message: expect.stringContaining('Unexpected error'),
-        //     },
-        // });
+        expect(actions).toHaveLength(1);
+        expect(actions).toContainEqual({
+            type: SET_GLOBAL_ALERT,
+            globalAlert: {
+                status: 'error',
+                message: expect.stringContaining('Unknown error'),
+            },
+        });
     });
 });
