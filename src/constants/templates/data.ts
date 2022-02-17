b8ecd73fe8afea9069eb45b0d16ecb278ed10705
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-02-17 20:51:46 +0800

    #41 sh3 template

--- src/constants/templates/data.ts
+++ src/constants/templates/data.ts
@@ -465,6 +465,14 @@ export const templateList: Record<CompanyCode, { filename: string; name: Transla
             },
         },
         {
+            filename: 'sh3',
+            name: {
+                en: 'Line 3',
+                'zh-Hans': '3号线',
+                'zh-Hant': '3號線',
+            },
+        },
+        {
             filename: 'sh5',
             name: {
                 en: 'Line 5 by Tianxiu11111',
