833d873c0218bc869fee57f91279c15a0a5089e3
Chito Wong <chi.to.wong@outlook.com>
2022-04-09 23:15:23 +0800

    #NOISSUE Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ set -eux
 npm run test:no-watch
 
 # git config
-git config user.name 'github-actions[bot]'
-git config user.email 'github-actions[bot]@users.noreply.github.com'
+git config --global user.name 'github-actions[bot]'
+git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 
 # variables
 export APP_NAME=rmg
