b3b34304f2f386c4f299b21cf13b2f7e6b379496
Chito Wong <chi.to.wong@outlook.com>
2022-05-14 15:50:32 +0800

    #355 Fix OSI link of MTR style terminal stations

--- src/svgs/mtr/station-icon.tsx
+++ src/svgs/mtr/station-icon.tsx
@@ -11,37 +11,40 @@ interface StationIconProps {
 export default function StationIcon(props: StationIconProps) {
     const { withinTransfer, outStationTransfer, isTerminal, isPassed, isPaidArea } = props;
 
+    const transforms = {
+        icon: {
+            v: withinTransfer <= 1 ? 0 : 18 * withinTransfer,
+        },
+        osiIcon: {
+            v: 18 * (outStationTransfer - 1),
+            x: isTerminal && withinTransfer ? 41 : 0,
+            y: isTerminal && withinTransfer ? 0 : 26,
+            scaleY: isTerminal && withinTransfer ? -1 : 1,
+        },
+    };
+
     return (
         <g stroke={isPassed ? 'var(--rmg-grey)' : 'var(--rmg-black)'} data-testid="station-icon-wrapper">
             {outStationTransfer && (
                 <path
-                    d={isTerminal ? 'M0,0H41' : 'M0,0V26'}
+                    d={isTerminal && withinTransfer ? 'M0,0H41' : 'M0,0V26'}
                     strokeWidth={2.69}
                     strokeDasharray={isPaidArea ? 0 : 2.5}
                 />
             )}
 
-            {withinTransfer <= 1 ? (
-                <circle r={8} className="rmg-stn__mtr" />
-            ) : (
+            <path
+                d={`M-8,0 v${transforms.icon.v} a8,8 0 0,0 16,0 v-${transforms.icon.v} a8,8 0 0,0 -16,0Z`}
+                className="rmg-stn__mtr"
+            />
+
+            {outStationTransfer && (
                 <path
-                    d={`M-8,0 v${18 * withinTransfer} a8,8 0 0,0 16,0 v-${18 * withinTransfer} a8,8 0 0,0 -16,0Z`}
+                    d={`M-8,0 v${transforms.osiIcon.v} a8,8 0 0,0 16,0 v-${transforms.osiIcon.v} a8,8 0 0,0 -16,0Z`}
                     className="rmg-stn__mtr"
+                    transform={`translate(${transforms.osiIcon.x},${transforms.osiIcon.y})scale(1,${transforms.osiIcon.scaleY})`}
                 />
             )}
-
-            {outStationTransfer &&
-                (outStationTransfer === 1 ? (
-                    <circle r={8} cy={26} className="rmg-stn__mtr" />
-                ) : (
-                    <path
-                        d={`M-8,0 v${18 * (outStationTransfer - 1)} a8,8 0 0,0 16,0 v-${
-                            18 * (outStationTransfer - 1)
-                        } a8,8 0 0,0 -16,0Z`}
-                        className="rmg-stn__mtr"
-                        transform={`translate(${isTerminal ? 41 : 0},${isTerminal ? -18 : 26})`}
-                    />
-                ))}
         </g>
     );
 }
