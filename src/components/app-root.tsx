937b32f16bf4463701b1ff1547e7116a1c46054a
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-09-26 22:02:17 +0800

    #445

--- src/components/app-root.tsx
+++ src/components/app-root.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box, Flex } from '@chakra-ui/react';
+import { RmgPage, RmgWindow } from '@railmapgen/rmg-components';
+import { useRootDispatch } from '../redux';
+import { setGlobalAlert } from '../redux/app/app-slice';
 import SvgRouter from '../svgs/svg-router';
 import SidePanel from './side-panel/side-panel';
 import PageHeader from './page-header/page-header';
 import WindowHeader from './root/window-header';
-import { Box, Flex } from '@chakra-ui/react';
 import GridTabs from './ag-grid/grid-tabs';
 import LoadingModal from './modal/loading-modal';
 import GlobalAlerts from './root/global-alerts';
-import { RmgPage, RmgWindow } from '@railmapgen/rmg-components';
 
 export default function AppRoot() {
+    const { t } = useTranslation();
+    const dispatch = useRootDispatch();
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            dispatch(
+                setGlobalAlert({
+                    status: 'info',
+                    message: t('rmpPromotion'),
+                    url: 'rmp',
+                    linkedApp: true,
+                })
+            );
+        }, 1000);
+    }, []);
+
     return (
         <RmgWindow>
             <LoadingModal />
