7c33b750abffca7428d0850cf43ae1e029233367
Chito Wong <chi.to.wong@outlook.com>
2022-11-13 13:46:48 +0800

    #40 Toggle on param selector view in PRD env

--- src/components/root/app-router.test.tsx
+++ src/components/root/app-router.test.tsx
@@ -28,129 +28,51 @@ const mockStore = createMockAppStore({
 });
 
 describe('AppRouter', () => {
-    describe('AppRouter - UAT toggle on', () => {
-        beforeEach(() => {
-            jest.spyOn(rmgRuntime, 'getEnv').mockReturnValue(RmgEnv.UAT);
-        });
-
-        afterEach(() => {
-            mockStore.clearActions();
-        });
-
-        it('Can render param selector view if param id is not specified', () => {
-            render(<AppRouter />, { store: mockStore, route: '/' });
-
-            const actions = mockStore.getActions();
-            expect(actions).toHaveLength(0);
-
-            expect(screen.getByRole('presentation', { name: 'Mock Param Selector View' })).toBeInTheDocument();
-        });
-
-        it('Can read param from localStorage and render app view if param is not loaded', () => {
-            createParamInLocalStorage('test-id');
-            render(<AppRouter />, { store: mockStore, route: '/?project=test-id' });
-
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setParamConfig', payload: { id: 'test-id' } });
-            expect(actions).toContainEqual(expect.objectContaining({ type: 'SET_FULL_PARAM' }));
-
-            expect(screen.getByRole('presentation', { name: 'Mock App View' })).toBeInTheDocument();
-        });
-
-        it('Can render param selector view if no param in localStorage matches URL param ID', () => {
-            render(<AppRouter />, { store: mockStore, route: '/?project=test-id-2' });
-
-            // TODO
-            // expect(screen.getByRole('presentation', { name: 'Mock Param Selector View' })).toBeInTheDocument();
-        });
-
-        it('Can render app view if param is loaded', async () => {
-            const mockStore = createMockAppStore({
-                ...realStore,
-                app: { ...realStore.app, paramConfig: { id: 'test-id' } },
-            });
-            render(<AppRouter />, { store: mockStore, route: '/?project=test-id' });
-
-            const actions = mockStore.getActions();
-            expect(actions).toHaveLength(0);
-
-            await screen.findByRole('presentation', { name: 'Mock App View' });
-        });
+    beforeEach(() => {
+        jest.spyOn(rmgRuntime, 'getEnv').mockReturnValue(RmgEnv.UAT);
     });
 
-    describe('AppRouter - PRD toggle off', () => {
-        beforeEach(() => {
-            jest.spyOn(rmgRuntime, 'getEnv').mockReturnValue(RmgEnv.PRD);
+    afterEach(() => {
+        mockStore.clearActions();
+    });
+
+    it('Can render param selector view if param id is not specified', () => {
+        render(<AppRouter />, { store: mockStore, route: '/' });
+
+        const actions = mockStore.getActions();
+        expect(actions).toHaveLength(0);
+
+        expect(screen.getByRole('presentation', { name: 'Mock Param Selector View' })).toBeInTheDocument();
+    });
+
+    it('Can read param from localStorage and render app view if param is not loaded', () => {
+        createParamInLocalStorage('test-id');
+        render(<AppRouter />, { store: mockStore, route: '/?project=test-id' });
+
+        const actions = mockStore.getActions();
+        expect(actions).toContainEqual({ type: 'app/setParamConfig', payload: { id: 'test-id' } });
+        expect(actions).toContainEqual(expect.objectContaining({ type: 'SET_FULL_PARAM' }));
+
+        expect(screen.getByRole('presentation', { name: 'Mock App View' })).toBeInTheDocument();
+    });
+
+    it('Can render param selector view if no param in localStorage matches URL param ID', () => {
+        render(<AppRouter />, { store: mockStore, route: '/?project=test-id-2' });
+
+        // TODO
+        // expect(screen.getByRole('presentation', { name: 'Mock Param Selector View' })).toBeInTheDocument();
+    });
+
+    it('Can render app view if param is loaded', async () => {
+        const mockStore = createMockAppStore({
+            ...realStore,
+            app: { ...realStore.app, paramConfig: { id: 'test-id' } },
         });
+        render(<AppRouter />, { store: mockStore, route: '/?project=test-id' });
 
-        afterEach(() => {
-            mockStore.clearActions();
-            window.localStorage.clear();
-        });
+        const actions = mockStore.getActions();
+        expect(actions).toHaveLength(0);
 
-        it('Can generate param and render app view if param id is not specified and no param in localStorage', () => {
-            render(<AppRouter />, { store: mockStore, route: '/' });
-
-            // update redux store
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual(expect.objectContaining({ type: 'app/setParamConfig' }));
-            expect(actions).toContainEqual(expect.objectContaining({ type: 'SET_FULL_PARAM' }));
-
-            expect(screen.getByRole('presentation', { name: 'Mock App View' })).toBeInTheDocument();
-        });
-
-        it('Can read first param from localStorage and render app view if param id is not specified', () => {
-            createParamInLocalStorage('test-id');
-            render(<AppRouter />, { store: mockStore, route: '/' });
-
-            // update redux store
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setParamConfig', payload: { id: 'test-id' } });
-            expect(actions).toContainEqual({
-                type: 'SET_FULL_PARAM',
-                fullParam: expect.objectContaining({ line_num: 'test-id' }),
-            });
-
-            expect(screen.getByRole('presentation', { name: 'Mock App View' })).toBeInTheDocument();
-        });
-
-        it('Can read param from localStorage and render app view if param is not loaded', () => {
-            createParamInLocalStorage('test-id');
-            render(<AppRouter />, { store: mockStore, route: '/?project=test-id' });
-
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setParamConfig', payload: { id: 'test-id' } });
-            expect(actions).toContainEqual(expect.objectContaining({ type: 'SET_FULL_PARAM' }));
-
-            expect(screen.getByRole('presentation', { name: 'Mock App View' })).toBeInTheDocument();
-        });
-
-        it('Can read first param from localStorage and render app selector view if no param in localStorage matches URL param ID', () => {
-            createParamInLocalStorage('test-id-1');
-            render(<AppRouter />, { store: mockStore, route: '/?project=test-id-2' });
-
-            // update redux store
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setParamConfig', payload: { id: 'test-id-1' } });
-            expect(actions).toContainEqual({
-                type: 'SET_FULL_PARAM',
-                fullParam: expect.objectContaining({ line_num: 'test-id-1' }),
-            });
-
-            expect(screen.getByRole('presentation', { name: 'Mock App View' })).toBeInTheDocument();
-        });
-
-        it('Can render app view if param is loaded', async () => {
-            const mockStore = createMockAppStore({
-                ...realStore,
-                app: { ...realStore.app, paramConfig: { id: 'test-id' } },
-            });
-            render(<AppRouter />, { store: mockStore, route: '/?project=test-id' });
-
-            const actions = mockStore.getActions();
-            expect(actions).toHaveLength(0);
-
-            await screen.findByRole('presentation', { name: 'Mock App View' });
-        });
+        await screen.findByRole('presentation', { name: 'Mock App View' });
     });
 });
--- src/components/root/app-router.tsx
+++ src/components/root/app-router.tsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import AppView from './app-view';
 import { useRootDispatch, useRootSelector } from '../../redux';
-import rmgRuntime, { RmgEnv } from '@railmapgen/rmg-runtime';
+import rmgRuntime from '@railmapgen/rmg-runtime';
 import { LanguageCode } from '@railmapgen/rmg-translate';
 import ParamSelectorView from '../param-selector-view';
 import { readParam } from '../../redux/app/action';
-import { getParamMap } from '../../util/param-manager-utils';
-import { nanoid } from 'nanoid';
 
 export default function AppRouter() {
     const dispatch = useRootDispatch();
 
     const { paramConfig } = useRootSelector(state => state.app);
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
     const paramId = searchParams.get('project');
 
     const [isLoaded, setIsLoaded] = useState(false);
@@ -25,32 +23,13 @@ export default function AppRouter() {
                 console.log('AppRouter:: Store param ID matches URL param ID. Rendering app view...');
                 setIsLoaded(true);
             } else {
-                if (rmgRuntime.getEnv() === RmgEnv.PRD) {
-                    const paramMap = getParamMap();
-                    const paramIds = Object.keys(paramMap);
-                    if (paramIds.length && !paramIds.includes(paramId)) {
-                        console.warn(
-                            'AppRouter:: URL param ID does not exist in localStorage. Reset to first param in localStorage in PRD env.'
-                        );
-                        const paramMap = getParamMap();
-                        setSearchParams({ project: Object.keys(paramMap)[0] });
-                        return;
-                    }
-                }
-
                 console.log(`AppRouter:: Reading param (ID=${paramId}) from localStorage`);
                 dispatch(readParam(paramId, rmgRuntime.getLanguage() as LanguageCode));
                 setIsLoaded(true);
             }
         } else {
-            if (rmgRuntime.getEnv() === RmgEnv.PRD) {
-                console.log('AppRouter:: Redirect to first param in localStorage in PRD env');
-                const paramMap = getParamMap();
-                setSearchParams({ project: Object.keys(paramMap)[0] ?? nanoid() });
-            } else {
-                console.log('AppRouter:: No URL param ID provided. Rendering param selector view...');
-                setIsLoaded(false);
-            }
+            console.log('AppRouter:: No URL param ID provided. Rendering param selector view...');
+            setIsLoaded(false);
         }
     }, [paramId]);
 
