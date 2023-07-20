6554bb71b964eac45d251ff5eaee4c96c693620a
Chito Wong <chi.to.wong@outlook.com>
2023-07-20 21:35:35 +0800

    #557 Remove language selector

--- src/components/root/window-header.tsx
+++ src/components/root/window-header.tsx
@@ -1,22 +1,16 @@
 import { useState } from 'react';
-import { Heading, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { Heading, HStack, IconButton } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
-import { MdHelp, MdTranslate } from 'react-icons/md';
+import { MdHelp } from 'react-icons/md';
 import HelpModal from '../modal/help-modal';
 import { RmgEnvBadge, RmgWindowHeader } from '@railmapgen/rmg-components';
 import rmgRuntime from '@railmapgen/rmg-runtime';
-import { LANGUAGE_NAMES, SUPPORTED_LANGUAGES, SupportedLanguageCode } from '@railmapgen/rmg-translate';
 
 export default function WindowHeader() {
     const { t } = useTranslation();
 
     const environment = rmgRuntime.getEnv();
     const appVersion = rmgRuntime.getAppVersion();
-
-    const handleChangeLanguage = (language: SupportedLanguageCode) => {
-        rmgRuntime.setLanguage(language);
-        rmgRuntime.getI18nInstance().changeLanguage(language);
-    };
 
     const [isHelpModalOpen, setIsHelpModalOpen] = useState(false);
 
@@ -28,19 +22,6 @@ export default function WindowHeader() {
             <RmgEnvBadge environment={environment} version={appVersion} />
 
             <HStack ml="auto">
-                {rmgRuntime.isStandaloneWindow() && (
-                    <Menu>
-                        <MenuButton as={IconButton} icon={<MdTranslate />} variant="ghost" size="sm" />
-                        <MenuList>
-                            {SUPPORTED_LANGUAGES.map(lang => (
-                                <MenuItem key={lang} onClick={() => handleChangeLanguage(lang)}>
-                                    {LANGUAGE_NAMES[lang][lang]}
-                                </MenuItem>
-                            ))}
-                        </MenuList>
-                    </Menu>
-                )}
-
                 <IconButton
                     size="sm"
                     variant="ghost"
