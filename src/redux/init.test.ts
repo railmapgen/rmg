e02e8c17dd5bcf4b746012c3f1bb568e92a1ecf2
Chito Wong <chi.to.wong@outlook.com>
2023-09-26 21:26:23 +0800

    #584 Remove legacy localStorage keys support

--- src/redux/init.test.ts
+++ src/redux/init.test.ts
@@ -3,7 +3,6 @@ import { createMockAppStore } from '../setupTests';
 import { initCanvasScale, initCanvasToShow, paramUpdateTrigger } from './init';
 import { CanvasType, LocalStorageKey, RMGParam, RmgStyle } from '../constants/constants';
 import { initParam } from './param/util';
-import { LanguageCode } from '@railmapgen/rmg-translate';
 import { vi } from 'vitest';
 
 const realStore = rootReducer.getState();
@@ -22,18 +21,6 @@ describe('ReduxInit', () => {
 
             const actions = mockStore.getActions();
             expect(actions).toContainEqual({ type: 'app/setCanvasScale', payload: 0.5 });
-        });
-
-        it('Can migrate canvasScale from old localStorage key as expected', () => {
-            window.localStorage.setItem('rmgScale', '0.5');
-            initCanvasScale(mockStore);
-
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setCanvasScale', payload: 0.5 });
-
-            // save to localStorage with new key
-            expect(window.localStorage.getItem(LocalStorageKey.CANVAS_SCALE)).toBe('0.5');
-            expect(window.localStorage.getItem('rmgScale')).toBeNull();
         });
     });
 
@@ -54,32 +41,12 @@ describe('ReduxInit', () => {
             expect(actions).toContainEqual({ type: 'app/setCanvasToShow', payload: ['destination', 'railmap'] });
         });
 
-        it('Can migrate canvasToShow from old localStorage key as expected', () => {
-            window.localStorage.setItem('rmgCanvas', 'railmap');
-            initCanvasToShow(mockStore);
-
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setCanvasToShow', payload: ['railmap'] });
-
-            // save to localStorage with new key
-            expect(window.localStorage.getItem(LocalStorageKey.CANVAS_TO_SHOW)).toBe('railmap');
-            expect(window.localStorage.getItem('rmgCanvas')).toBeNull();
-        });
-
         it('Can ignore invalid canvas from localStorage', () => {
             window.localStorage.setItem(LocalStorageKey.CANVAS_TO_SHOW, 'invalid');
             initCanvasToShow(mockStore);
 
             const actions = mockStore.getActions();
             expect(actions).toHaveLength(0);
-        });
-
-        it('Can parse string type canvasToShow from localStorage key as expected', () => {
-            window.localStorage.setItem(LocalStorageKey.CANVAS_TO_SHOW, 'railmap');
-            initCanvasToShow(mockStore);
-
-            const actions = mockStore.getActions();
-            expect(actions).toContainEqual({ type: 'app/setCanvasToShow', payload: ['railmap'] });
         });
     });
 
