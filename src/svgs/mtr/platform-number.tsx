30c69c4a65dd1f22d2427eb750d959df6cfcf00f
Chito Wong <chi.to.wong@outlook.com>
2024-02-10 00:58:48 +0800

    #625 Use Vegur-Regular to display MTR platform number

--- src/svgs/mtr/platform-number.tsx
+++ src/svgs/mtr/platform-number.tsx
@@ -4,13 +4,22 @@ interface PlatformNumberProps {
     num: string | false;
 }
 
+export const FONTS = ['Vegur-Regular'];
+
 export default memo(
     function PlatformNumber(props: PlatformNumberProps) {
         const { num } = props;
         return (
             <>
                 <circle cx={0} cy={0} r={60} fill="var(--rmg-theme-colour)" />
-                <text className="rmg-name__zh" dy={0} textAnchor="middle" fontSize={100} fill="#fff">
+                <text
+                    fontFamily={FONTS.join(', ')}
+                    dominantBaseline="central"
+                    dy={0}
+                    textAnchor="middle"
+                    fontSize={100}
+                    fill="#fff"
+                >
                     {num}
                 </text>
             </>
