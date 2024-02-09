5221c33b46b19fdcdc185db8e24961840440f2b5
Chito Wong <chi.to.wong@outlook.com>
2024-02-10 00:58:48 +0800

    #625 Use Vegur-Regular to display MTR platform number

--- src/svgs/mtr/index.tsx
+++ src/svgs/mtr/index.tsx
@@ -2,13 +2,14 @@ import { CanvasType } from '../../constants/constants';
 import { ReactNode } from 'react';
 import DestinationMTR from './destination-mtr';
 import RailMapMTR from './railmap-mtr';
-import { FONTS } from './station/station-name/station-name';
+import { FONTS as StationNameFonts } from './station/station-name/station-name';
+import { FONTS as PlatformNumFonts } from './platform-number';
 
 const mtrSvgs: Partial<Record<CanvasType, ReactNode>> = {
     destination: <DestinationMTR />,
     railmap: <RailMapMTR />,
 };
 
-export const mtrFonts = FONTS;
+export const mtrFonts = [...StationNameFonts, ...PlatformNumFonts];
 
 export default mtrSvgs;
