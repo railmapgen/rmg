54bf43f9605228d558a05bbb9afcfd837dbdf14a
Chito Wong <chi.to.wong@outlook.com>
2025-03-24 22:13:52 +0800

    #707 Load FontFace list into document before rendering SVGs

--- src/svgs/svg-router.tsx
+++ src/svgs/svg-router.tsx
@@ -1,11 +1,8 @@
-import { useEffect } from 'react';
-import { CanvasType, Events, RmgStyle } from '../constants/constants';
+import { CanvasType } from '../constants/constants';
 import { useRootSelector } from '../redux';
 import { Flex } from '@chakra-ui/react';
 import useCanvasMap from './use-canvas-map';
 import { RmgErrorBoundary, RmgLoader } from '@railmapgen/rmg-components';
-import rmgRuntime from '@railmapgen/rmg-runtime';
-import { STYLE_CONFIG } from './config';
 
 const style = {
     position: 'relative',
@@ -21,31 +18,11 @@ const style = {
     },
 };
 
-const loadFontsByStyle = async (style: RmgStyle, signal: AbortSignal) => {
-    const fonts = (await STYLE_CONFIG[style].fonts?.()) ?? [];
-    await Promise.all(fonts.map(font => rmgRuntime.loadFont(font)));
-    if (!signal.aborted) {
-        document
-            .querySelector<HTMLLinkElement>('#css_share')
-            ?.setAttribute('href', import.meta.env.BASE_URL + `styles/share_${style}.css`);
-    }
-};
-
 export default function SvgRouter() {
     const { canvasToShow, canvasScale } = useRootSelector(state => state.app);
     const { svg_height: svgHeight, style: rmgStyle } = useRootSelector(state => state.param);
 
     const canvasMap = useCanvasMap(rmgStyle);
-
-    useEffect(() => {
-        const controller = new AbortController();
-        loadFontsByStyle(rmgStyle, controller.signal).then();
-        rmgRuntime.event(Events.STYLE_CHANGE, { style: rmgStyle });
-
-        return () => {
-            controller.abort();
-        };
-    }, [rmgStyle]);
 
     const filteredCanvas = (Object.keys(canvasMap) as CanvasType[]).filter(canvas => canvasToShow.includes(canvas));
     const scaledHeight = svgHeight * canvasScale;
